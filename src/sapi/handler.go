b331661eecb3af41daaaa922dde42972a093eb55
ryuta <ryuta.hara@topgate.co.jp>
2017-09-28 14:33:22 +0900

    Update: エンティティ変更

--- src/sapi/handler.go
+++ src/sapi/handler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"google.golang.org/appengine"
@@ -19,6 +21,8 @@ type User struct {
 	Visits    float64
 	LastVisit time.Time
 	Birthday  time.Time
+	Mail      []string
+	UserID    int64
 }
 
 type UserIndex struct {
@@ -28,10 +32,17 @@ type UserIndex struct {
 	Visits    float64
 	LastVisit time.Time
 	Birthday  time.Time
+	Mail      string
+	UserID    string
 }
 
 func PutSamples(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
+	mails := [][]string{
+		{"mail1@sample.com", "mail2@sample.com"},
+		{"mail2@sample.com", "mail3@sample.com"},
+		{"mail4@sample.com", "mail5@sample.com"},
+	}
 
 	users := make(map[*datastore.Key]User)
 	for i := 0; i < 500; i++ {
@@ -42,6 +53,8 @@ func PutSamples(w http.ResponseWriter, r *http.Request) {
 			Visits:    float64(rand.Int63n(100)),
 			LastVisit: time.Now(),
 			Birthday:  time.Date(1990, time.January, rand.Intn(29), 0, 0, 0, 0, time.UTC),
+			Mail:      mails[i%3],
+			UserID:    int64(i + 1),
 		}
 		newKey, err := datastore.Put(ctx, key, &usr)
 		if err != nil {
@@ -64,6 +77,8 @@ func PutSamples(w http.ResponseWriter, r *http.Request) {
 			Visits:    usr.Visits,
 			LastVisit: usr.LastVisit,
 			Birthday:  usr.Birthday,
+			Mail:      strings.Join(usr.Mail, " "),
+			UserID:    fmt.Sprint(usr.UserID),
 		}
 		id := fmt.Sprint(key.IntID())
 		if _, err := index.Put(ctx, id, usrIndex); err != nil {
@@ -84,9 +99,17 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	userName := r.FormValue("name")
+	mail := r.FormValue("mail")
+	userID := r.FormValue("userid")
 	cursor := r.FormValue("cursor")
-	query := fmt.Sprintf("Name = %s", userName)
+
+	var query string
+	if mail != "" {
+		query += fmt.Sprintf(`Mail = "%s"`, mail)
+	}
+	if userID != "" {
+		query += fmt.Sprintf(`UserID = "%s"`, userID)
+	}
 	log.Infof(ctx, "Query: %v", query)
 
 	option := &search.SearchOptions{
@@ -104,24 +127,38 @@ func Search(w http.ResponseWriter, r *http.Request) {
 
 	ite := index.Search(ctx, query, option)
 
-	type result struct {
-		Users  []*UserIndex
-		Cursor search.Cursor
-	}
-	rslt := new(result)
+	var ids []int64
 	for {
-		usrIndex := new(UserIndex)
-		id, err := ite.Next(usrIndex)
+		id, err := ite.Next(nil)
 		if err == search.Done {
 			break
 		}
 		if err != nil {
 			http.Error(w, err.Error(), 500)
 		}
-		usrIndex.ID = id
-		rslt.Users = append(rslt.Users, usrIndex)
+		i, _ := strconv.ParseInt(id, 10, 64)
+		ids = append(ids, i)
 	}
-	log.Infof(ctx, "%v", ite.Cursor())
+
+	var keys []*datastore.Key
+	for _, id := range ids {
+		key := datastore.NewKey(ctx, "User", "", id, nil)
+		keys = append(keys, key)
+	}
+	users := make([]User, len(keys))
+	if err := datastore.GetMulti(ctx, keys, users); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	type result struct {
+		Users  []User
+		Cursor search.Cursor
+	}
+	rslt := result{
+		Users:  users,
+		Cursor: ite.Cursor(),
+	}
 	rslt.Cursor = ite.Cursor()
 
 	w.Header().Set("Content-Type", "application/json; charset=utf8")
